b5fcee54deee352477ca3d3d3342df7cd0a641bb
Erica-Cradeur <79936012+Erica-Cradeur@users.noreply.github.com>
2023-08-11 10:47:30 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ function App() {
   return (
     <div className="App">
       <div className="navbar">
-        <a href="#about">About Me</a>
+        <a href="#aboutme">About Me</a>
         <a href="#skills">Skills</a>
         <a href="#projects">Projects</a>
-        <a href="#contact">Contact Me</a>
+        <a href="#contacts">Contact Me</a>
       </div>
     </div>
+    
   );
 }
 
